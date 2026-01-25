beb290f9b18c07a6a1a9b8ff36f0502410116370
G-xy805 <1473388751@qq.com>
2026-01-25 21:08:24 +0800

    Fix LIGHT_DARK_MODE type export issue

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,7 +1,7 @@
 export const PAGE_SIZE = 8;
-
 export const LIGHT_MODE = "light",
 	DARK_MODE = "dark";
+export const LIGHT_DARK_MODE = typeof LIGHT_MODE | typeof DARK_MODE;
 export const DEFAULT_THEME = LIGHT_MODE; // 仅作为向后兼容的默认值，实际使用 siteConfig.themeColor.defaultMode
 
 // Wallpaper modes
